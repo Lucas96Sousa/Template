261e31a9b954f21ab3b5282272732271ebd3d53a
Lucas Sousa <37560590+Lucas96Sousa@users.noreply.github.com>
2020-04-14 21:53:07 -0300

    Commit Inicial

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+// Controllers
+import UserController from './app/controllers/UserController';
+
+const routes = Router();
+
+routes.post('/users', UserController.store);
+
+export default routes;
